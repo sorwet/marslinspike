ec949627ff5b7fd295e27c619ae6300220125dcc
nartesfasrum <nart@tirefire.pw>
2021-03-08 19:03:05 -0600

    Quinnat proof of concept
    let's take this a step at a time

--- main.py
+++ main.py
@@ -1,43 +1,20 @@
 import asyncio, configparser, quinnat
-from pysignald_async.api import JsonAddressv1, JsonMessageEnvelopev1
 
 config = configparser.ConfigParser()
 config.read('default.ini')
 
-signalUsername = config['Signal']['signalUsername']
-signaldSocketPath = config['Signal']['signaldSocketPath']
-urbitUrl = config['Urbit']['urbitUrl']
-urbitId = config['Urbit']['urbitId']
-urbitCode = config['Urbit']['urbitCode']
-urbitBridgeChat = config['Urbit']['urbitBridgeChat']
+signalUsername = config['SIGNAL']['signalUsername']
+signaldSocketPath = config['SIGNAL']['signaldSocketPath']
+urbitUrl = config['URBIT']['urbitUrl']
+urbitId = config['URBIT']['urbitId']
+urbitCode = config['URBIT']['urbitCode']
+urbitBridgeChat = config['URBIT']['urbitBridgeChat']
+urbitHost = config['URBIT']['urbitHost']
 
-urbitClient = Quinnat(urbitUrl, urbitId, urbitCode)
+urbitClient = quinnat.Quinnat(urbitUrl, urbitId, urbitCode)
+print("Quinnat initialised. Connecting to ship {} @ {} ...".format(urbitId, urbitUrl))
 
-
-class RelayBot(SignaldAPI):
-
-    def handle_envelope(self, payload):
-
-        message = envelope.dataMessage
-        source = envelope.source.number
-
-        if message is not None and source is not None:
-            username = envelope.username
-            asyncio.create_task(
-                    urbitClient.post_message(
-                        urbitHost, 
-                        urbitBridgeChat, 
-                        {
-                            "text": "Got message from {source}: {message.body}"
-                        }
-                    )
-            )
-
-async def main():
-    urbitClient.connect()
-
-    loop = asyncio.get_running_loop()
-    _, signald = await loop.create_unix_connection(SignaldAPI, path=signaldSocketPath)
-    await signald.subscribe(username=signalUsername)
-
-asyncio.run(main())
+urbitClient.connect()
+print("Connected to ship {}.".format(urbitId))
+print("Posting message in {}...".format(urbitBridgeChat))
+urbitClient.post_message(urbitHost, urbitBridgeChat, {"text": "Sending test message."})
